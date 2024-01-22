32a89351829fe0f32bea81158078669eab31c8f9
Roman Hnatiuk <roman.hnatiuk02@gmail.com>
2024-01-22 17:48:04 +0200

    fix

--- linkedin_auto_post.js
+++ linkedin_auto_post.js
@@ -25,9 +25,30 @@ async function fetchAutopost() {
 
 async function postToLinkedIn(postText, mediaLink) {
   try {
-    const shareApiUrl = 'https://api.linkedin.com/v2/shares';
+    const meApiUrl = 'https://api.linkedin.com/v2/me';
+    const meResponse = await fetch(meApiUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Restli-Protocol-Version': '2.0.0',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    const meData = await meResponse.json();
+    const memberID = meData.id;
+
+    const shareApiUrl = `https://api.linkedin.com/v2/people/${memberID}/shares`;
 
     const postData = {
+      owner: `urn:li:person:${memberID}`,
+      subject: 'Your Post Title',
+      text: {
+        text: postText,
+      },
+      distribution: {
+        linkedInDistributionTarget: {},
+      },
       content: {
         contentEntities: [
           {
@@ -36,14 +57,7 @@ async function postToLinkedIn(postText, mediaLink) {
             },
           },
         ],
-        title: 'Your Post Title',
-        description: {
-          text: postText,
-        },
         shareMediaCategory: 'ARTICLE',
-      },
-      distribution: {
-        linkedInDistributionTarget: {},
       },
     };
 
